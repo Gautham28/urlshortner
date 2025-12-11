f519dbdcebc0677cce4ad467fb15aa92b6687333
Your Name <gauthamk2003@gmail.com>
2025-12-11 15:56:43 +0530

    fin

--- controllers/url.js
+++ controllers/url.js
@@ -1,31 +1,33 @@
-const { nanoid } = require("nanoid");
-const URL = require('../models/url');
+const shortid = require("shortid");
+const URL = require("../models/url");
 
-async function handleGenerateNewShortURL(req, res){
-    const body = req.body;
-    if(!body.url) return res.status(400).json({error: "url is requiresd"})
-    const shortID = nanoid(8);
-    await URL.create({
-        shortID: shortID,
-        redirectURL: body.url,
-        visitHistory: [],
-    });
+async function handleGenerateNewShortURL(req, res) {
+  const body = req.body;
+  if (!body.url) return res.status(400).json({ error: "url is required" });
+  const shortID = shortid();
 
-    return res.render('home', {
-        id: shortID 
-    })
+  await URL.create({
+    shortId: shortID,
+    redirectURL: body.url,
+    visitHistory: [],
+    createdBy: req.user._id,
+  });
 
+  return res.render("home", {
+    id: shortID,
+  });
 }
 
 async function handleGetAnalytics(req, res) {
-    const shortID = req.params.shortID;
-    const result = await URL.findOne({shortID});
-    return res.json({totalclicks:result.visitHistory.length, 
-        analytics: result.visitHistory,
-});
+  const shortId = req.params.shortId;
+  const result = await URL.findOne({ shortId });
+  return res.json({
+    totalClicks: result.visitHistory.length,
+    analytics: result.visitHistory,
+  });
 }
 
 module.exports = {
-    handleGenerateNewShortURL,
-    handleGetAnalytics,
+  handleGenerateNewShortURL,
+  handleGetAnalytics,
 };
